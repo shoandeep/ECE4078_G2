5601cb13fe7353aae5c41aaa616c7d3e509e9d2a
Shoandeep Radhakrishnan <93181982+shoandeep@users.noreply.github.com>
2023-10-16 07:40:58 +0800

    Added Servo & Buzzer Functionality

---
+++ AlphaBot.py
@@ -0,0 +1,201 @@
+import RPi.GPIO as GPIO
+import time
+import math
+import smbus
+
+#Class for servo controller PCA9685
+class PCA9685:
+
+  # Registers/etc.
+  __SUBADR1            = 0x02
+  __SUBADR2            = 0x03
+  __SUBADR3            = 0x04
+  __MODE1              = 0x00
+  __PRESCALE           = 0xFE
+  __LED0_ON_L          = 0x06
+  __LED0_ON_H          = 0x07
+  __LED0_OFF_L         = 0x08
+  __LED0_OFF_H         = 0x09
+  __ALLLED_ON_L        = 0xFA
+  __ALLLED_ON_H        = 0xFB
+  __ALLLED_OFF_L       = 0xFC
+  __ALLLED_OFF_H       = 0xFD
+
+  def __init__(self, address=0x40, debug=False):
+    self.bus = smbus.SMBus(1)
+    self.address = address
+    self.debug = debug
+    if (self.debug):
+      print("Reseting PCA9685")
+    self.write(self.__MODE1, 0x00)
+	
+  def write(self, reg, value):
+    "Writes an 8-bit value to the specified register/address"
+    self.bus.write_byte_data(self.address, reg, value)
+    if (self.debug):
+      print("I2C: Write 0x%02X to register 0x%02X" % (value, reg))
+	  
+  def read(self, reg):
+    "Read an unsigned byte from the I2C device"
+    result = self.bus.read_byte_data(self.address, reg)
+    if (self.debug):
+      print("I2C: Device 0x%02X returned 0x%02X from reg 0x%02X" % (self.address, result & 0xFF, reg))
+    return result
+	
+  def setPWMFreq(self, freq):
+    "Sets the PWM frequency"
+    prescaleval = 25000000.0    # 25MHz
+    prescaleval /= 4096.0       # 12-bit
+    prescaleval /= float(freq)
+    prescaleval -= 1.0
+    if (self.debug):
+      print("Setting PWM frequency to %d Hz" % freq)
+      print("Estimated pre-scale: %d" % prescaleval)
+    prescale = math.floor(prescaleval + 0.5)
+    if (self.debug):
+      print("Final pre-scale: %d" % prescale)
+
+    oldmode = self.read(self.__MODE1);
+    newmode = (oldmode & 0x7F) | 0x10        # sleep
+    self.write(self.__MODE1, newmode)        # go to sleep
+    self.write(self.__PRESCALE, int(math.floor(prescale)))
+    self.write(self.__MODE1, oldmode)
+    time.sleep(0.005)
+    self.write(self.__MODE1, oldmode | 0x80)
+
+  def setPWM(self, channel, on, off):
+    "Sets a single PWM channel"
+    self.write(self.__LED0_ON_L+4*channel, on & 0xFF)
+    self.write(self.__LED0_ON_H+4*channel, on >> 8)
+    self.write(self.__LED0_OFF_L+4*channel, off & 0xFF)
+    self.write(self.__LED0_OFF_H+4*channel, off >> 8)
+    if (self.debug):
+      print("channel: %d  LED_ON: %d LED_OFF: %d" % (channel,on,off))
+	  
+  def setServoPulse(self, channel, pulse):
+    "Sets the Servo Pulse,The PWM frequency must be 50HZ"
+    pulse = int(pulse*4096/20000)      #PWM frequency is 50HZ,the period is 20000us
+    self.setPWM(channel, 0, pulse)
+
+#Class for AlphaBot
+class AlphaBot(object):
+	
+	def __init__(self,in1=13,in2=12,ena=6,in3=21,in4=20,enb=26):
+		self.pwm = PCA9685(0x40, debug=True)
+		self.pwm.setPWMFreq(50)
+		self.IN1 = in1
+		self.IN2 = in2
+		self.IN3 = in3
+		self.IN4 = in4
+		self.ENA = ena
+		self.ENB = enb
+		self.PA  = 20
+		self.PB  = 20
+		self.BUZ = 4
+
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(self.BUZ,GPIO.OUT)
+
+		GPIO.setup(self.IN1,GPIO.OUT)
+		GPIO.setup(self.IN2,GPIO.OUT)
+		GPIO.setup(self.IN3,GPIO.OUT)
+		GPIO.setup(self.IN4,GPIO.OUT)
+		GPIO.setup(self.ENA,GPIO.OUT)
+		GPIO.setup(self.ENB,GPIO.OUT)
+		self.PWMA = GPIO.PWM(self.ENA,500)
+		self.PWMB = GPIO.PWM(self.ENB,500)
+		self.PWMA.start(self.PA)
+		self.PWMB.start(self.PB)
+		self.stop()
+
+	def forward(self):
+		self.PWMA.ChangeDutyCycle(self.PA)
+		self.PWMB.ChangeDutyCycle(self.PB)
+		GPIO.output(self.IN1,GPIO.HIGH)
+		GPIO.output(self.IN2,GPIO.LOW)
+		GPIO.output(self.IN3,GPIO.HIGH)
+		GPIO.output(self.IN4,GPIO.LOW)
+
+	def stop(self):
+		self.PWMA.ChangeDutyCycle(0)
+		self.PWMB.ChangeDutyCycle(0)
+		GPIO.output(self.IN1,GPIO.LOW)
+		GPIO.output(self.IN2,GPIO.LOW)
+		GPIO.output(self.IN3,GPIO.LOW)
+		GPIO.output(self.IN4,GPIO.LOW)
+
+	def backward(self):
+		self.PWMA.ChangeDutyCycle(self.PA)
+		self.PWMB.ChangeDutyCycle(self.PB)
+		GPIO.output(self.IN1,GPIO.LOW)
+		GPIO.output(self.IN2,GPIO.HIGH)
+		GPIO.output(self.IN3,GPIO.LOW)
+		GPIO.output(self.IN4,GPIO.HIGH)
+
+	def left(self):
+		self.PWMA.ChangeDutyCycle(30)
+		self.PWMB.ChangeDutyCycle(30)
+		GPIO.output(self.IN1,GPIO.LOW)
+		GPIO.output(self.IN2,GPIO.HIGH)
+		GPIO.output(self.IN3,GPIO.HIGH)
+		GPIO.output(self.IN4,GPIO.LOW)
+
+	def right(self):
+		self.PWMA.ChangeDutyCycle(30)
+		self.PWMB.ChangeDutyCycle(30)
+		GPIO.output(self.IN1,GPIO.HIGH)
+		GPIO.output(self.IN2,GPIO.LOW)
+		GPIO.output(self.IN3,GPIO.LOW)
+		GPIO.output(self.IN4,GPIO.HIGH)
+		
+	def setPWMA(self,value):
+		self.PA = value
+		self.PWMA.ChangeDutyCycle(self.PA)
+
+	def setPWMB(self,value):
+		self.PB = value
+		self.PWMB.ChangeDutyCycle(self.PB)	
+		
+	def setMotor(self, left, right):
+		if((left >= 0) and (left <= 100)):
+			GPIO.output(self.IN1,GPIO.HIGH)
+			GPIO.output(self.IN2,GPIO.LOW)
+			self.PWMA.ChangeDutyCycle(left)
+		elif((left < 0) and (left >= -100)):
+			GPIO.output(self.IN1,GPIO.LOW)
+			GPIO.output(self.IN2,GPIO.HIGH)
+			self.PWMA.ChangeDutyCycle(0 - left)
+        
+		if((right >= 0) and (right <= 100)):
+			GPIO.output(self.IN3,GPIO.HIGH)
+			GPIO.output(self.IN4,GPIO.LOW)
+			self.PWMB.ChangeDutyCycle(right)
+		elif((right < 0) and (right >= -100)):
+			GPIO.output(self.IN3,GPIO.LOW)
+			GPIO.output(self.IN4,GPIO.HIGH)
+			self.PWMB.ChangeDutyCycle(0 - right)
+
+	def setBuzzer(self, buzz_val):
+		if (buzz_val==True):
+			GPIO.output(self.BUZ,GPIO.HIGH)
+		elif (buzz_val==False):
+			GPIO.output(self.BUZ,GPIO.LOW)
+			
+	def servo(self, input_pulse):
+		self.pwm.setServoPulse(0,input_pulse)  
+
+if __name__=='__main__':
+
+	Ab = AlphaBot()
+	Ab.forward()
+	Ab.servo(500)
+	time.sleep(0.2)
+	Ab.setBuzzer(True)
+	time.sleep(1)
+	Ab.setBuzzer(False)
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		GPIO.cleanup()
